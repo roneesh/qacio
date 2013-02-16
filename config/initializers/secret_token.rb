d562dfc79a20a22f2d6c227fb7ce7f752aba507d
Roneesh Vashisht <roneesh@gmail.com>
2013-02-16 16:34:32 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Qacio::Application.config.secret_token = 'dc9a965ebfdb4b911fccf663b4b28b74fbeac1fea97d4a8e227194b84c41c33bdc1bebef58045cad92e420d24b879e5869fc7ff7a5b84b251e83c0086f7833b3'
