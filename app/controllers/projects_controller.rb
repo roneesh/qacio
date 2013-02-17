d9eb8217edc0967cbcabbef38ee422383962a4c4
Roneesh Vashisht <roneesh@gmail.com>
2013-02-16 19:26:34 -0600

    user_id now being assigned to projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -44,10 +44,11 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(params[:project])
+    @project.user_id = current_user.id
 
     respond_to do |format|
       if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.html { redirect_to user_project_path(@user.id, @project.id), notice: 'Project was successfully created.' }
         format.json { render json: @project, status: :created, location: @project }
       else
         format.html { render action: "new" }
